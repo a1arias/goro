25eb16b6dccd6d14152a73b3e223877ec71424bb
Adrian Arias <aarias239@gmail.com>
2014-09-06 17:04:59 -0700

    fixup staticfiles finder

--- goro/settings.py
+++ goro/settings.py
@@ -82,10 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = 'static'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    'goro/static'
+    'goro/static',
 )
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
