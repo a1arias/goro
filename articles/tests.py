2dbd62227ff8a312cff62a2eb20c2ec20766c438
Adrian Arias <aarias239@gmail.com>
2014-09-08 01:04:29 -0700

    unit test for no articles and related bugfix for the index template

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class ArticalIndexViewTests(TestCase):
+    def test_index_view_with_no_articles(self):
+        """
+        If no articles exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('articles:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No Articles")
+        self.assertQuerysetEqual(response.context['articles'], [])
