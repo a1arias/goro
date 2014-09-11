d30660c30755705baa3651cbfaf49a4ad7a3d994
Adrian Arias <aarias239@gmail.com>
2014-09-10 21:12:10 -0700

    added placeholder for home page. migrated style to goro.

--- goro/urls.py
+++ goro/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from goro import views
 
 urlpatterns = patterns(
     '',
@@ -8,7 +9,7 @@ urlpatterns = patterns(
     #url(r'^$', 'goro.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('articles.urls'), name='home'),
+    url(r'^$', views.index, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^articles/', include('articles.urls', namespace='articles'), name='articles'),
