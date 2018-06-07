15c58f90c59ae8e5d94739619d0fcb72213f11a2
Adrian Arias <aarias239@gmail.com>
2018-06-06 17:07:03 -0700

    dynamic word count on home page
    * added dynamic word count via nltk
    * replaced spaces with tab

--- articles/views.py
+++ articles/views.py
@@ -4,35 +4,37 @@ from articles.models import Article
 from articles.utils import unslugify
 
 def index(request):
-    articles = Article.objects.all().order_by('-pub_date')[:10]
-    context = {'articles': articles}
-    return render(request, 'articles/index.html', context)
+	articles = Article.objects.all().order_by('-pub_date')[:10]
+	context = {
+		'articles': articles
+	}
+	return render(request, 'articles/index.html', context)
 
 def year_archive(request, year):
-    #articles = get_list_or_404(Article, pub_date__year=year)
-    articles = Article.objects.filter(pub_date__year=year)
-    context = {
-        'articles': articles,
-        'year': year
-    }
-    return render(request, 'articles/year_archive.html', context)
+	#articles = get_list_or_404(Article, pub_date__year=year)
+	articles = Article.objects.filter(pub_date__year=year)
+	context = {
+		'articles': articles,
+		'year': year
+	}
+	return render(request, 'articles/year_archive.html', context)
 
 def month_archive(request, year, month):
-    articles = Article.objects.filter(pub_date__year=year, pub_date__month=month)
-    context = {
-        'articles': articles,
-        'year': year,
-        'month': month
-    }
-    return render(request, 'articles/month_archive.html', context)
+	articles = Article.objects.filter(pub_date__year=year, pub_date__month=month)
+	context = {
+		'articles': articles,
+		'year': year,
+		'month': month
+	}
+	return render(request, 'articles/month_archive.html', context)
 
 def detail(request, slug):
-    """
-    Slug can be 'something-like-this'
-    """
-    article = get_object_or_404(Article, slug__exact=slug)
-    context = {
-        'article': article
-    }
-    template = 'articles/detail.html'
-    return render(request, template, context)
+	"""
+	Slug can be 'something-like-this'
+	"""
+	article = get_object_or_404(Article, slug__exact=slug)
+	context = {
+		'article': article
+	}
+	template = 'articles/detail.html'
+	return render(request, template, context)
--- goro/views.py
+++ goro/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
+from articles.models import Article
 from goro.models import Page
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
 
 def index(request):
 
-    p = Page.objects.get(name__iexact='home')
+	p = Page.objects.get(name__iexact='home')
 
-    return render(request, 'goro/index.html',
-                  {'page': p})
+	all_article_contents = [x["headline"] + " " + x["content"] for x in Article.objects.values("content", "headline")]
+	english_stops = set(stopwords.words("english"))
+	tokenizer = RegexpTokenizer("[\w]+")
+	token_counts = {}
+	for article in all_article_contents:
+		tokens = tokenizer.tokenize(" ".join(filter(lambda x: reduce(
+			lambda x, y: x and y, map(
+				lambda x: x,
+				[
+					x[0:1] != ' ',
+					x[0:2] != "..",
+					x[0:1] != '+',
+					x[0:1] != '-',
+					x[0:1] != '~',
+					x[0:1] != '=',
+					x[0:2] != "::",
+					x[0:1] != ''
+				])
+		), article.split('\r\n'))))
+		tokens = [t for t in tokens if t not in english_stops]
+		token_counts_map =  map(lambda x: (x, 1), tokens)
+		for x in token_counts_map:
+			if(token_counts.get(x[0])):
+				token_counts[x[0]] = token_counts[x[0]] + x[1]
+			else:
+				token_counts[x[0]] = 1
+
+	word_counts = sorted(token_counts.iteritems(), key=lambda x: x[1], reverse=True)
+
+	context = {
+		"page": p,
+        "word_counts": word_counts[:10]
+	}
+
+	return render(request, 'goro/index.html', context)
